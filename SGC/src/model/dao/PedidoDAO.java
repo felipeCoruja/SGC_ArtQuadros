0b81583900b71959f125c615170c9f74f164d847
Felipe Costa <felipemdb5@gmail.com>
2021-10-08 17:17:17 -0300

    Método salvar() da classe PedidoDAO concluido

--- SGC/src/model/dao/PedidoDAO.java
+++ SGC/src/model/dao/PedidoDAO.java
@@ -19,38 +19,14 @@ import javax.swing.JOptionPane;
  * @author felip
  */
 public class PedidoDAO {
-    
-//    public void salvar(Pedido p) throws ClassNotFoundException{
-//        Connection con = ConnectionFactory.getConnection();
-//        PreparedStatement stmt = null;
-//        
-//        try {
-//            stmt = con.prepareStatement("INSERT INTO pedido(id,altura,largura,valor_unitario,tipo,quantidade)"
-//                                      + " VALUES(?,?,?,?,?,?)");
-//            stmt.setInt(1, p.getId());
-//            stmt.setDouble(2, p.getAltura());
-//            stmt.setDouble(3, p.getLargura());
-//            stmt.setDouble(4, p.getValorUnitario());
-//            stmt.setString(5, p.getTipo());
-//            stmt.setInt(6, p.getQuantidade());
-//            
-//            stmt.executeUpdate();
-//            
-//        } catch (SQLException ex) {
-//            Logger.getLogger(PedidoDAO.class.getName()).log(Level.SEVERE, null, ex);
-//            JOptionPane.showMessageDialog(null, "Erro ao salvar pedido na Classe PedidoDAO :"+ex);
-//        }finally{
-//            ConnectionFactory.closeConnection(con, stmt);
-//        }
-//    }
-//    
+       
     public void salvar(List<Pedido> listaPedido) throws ClassNotFoundException{
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
         
         try {
             
-            for(int i =0; i<listaPedido.size();i++){   
+            for(int i=0;i<listaPedido.size();i++){   
                 
                 stmt = con.prepareStatement("INSERT INTO pedido(id,altura,largura,valor_unitario,tipo,quantidade)"
                                       + " VALUES(?,?,?,?,?,?,?)");
@@ -63,10 +39,39 @@ public class PedidoDAO {
                 stmt.setString(7, listaPedido.get(i).getDescricao());
 
                 stmt.executeUpdate();
+                stmt = null;
                 
+                int cont = 1;
+                for(int j = listaPedido.get(i).getListaPaspatu().size()-1;j>=0;j--){
+                    stmt = con.prepareStatement("INSERT INTO pedido_moldura(id_pedido,id_moldura,ordem_moldura_paspatu)"
+                                                + "VALUES(?,?,?)");
+                    stmt.setInt(1, listaPedido.get(i).getId());
+                    stmt.setString(2, listaPedido.get(i).getMoldura());
+                    stmt.setInt(3, cont);
+                    cont++;
+                    
+                    stmt.executeUpdate();
+                }
+                
+                stmt = con.prepareStatement("INSERT INTO pedido_vidro(id_pedido,id_vidro,entre_vidros)VALUES(?,?,?)");
+                stmt.setInt(1, listaPedido.get(i).getId());
+                stmt.setInt(2, listaPedido.get(i).getIdVidro());
+                String sn = "N";
+                if(listaPedido.get(i).isEntreVidros()){
+                    sn = "S";
+                }
+                stmt.setString(3,sn);
+                stmt.executeUpdate();
+                
+                
+                stmt = con.prepareStatement("INSERT INTO pedido_eucatex(id_pedido,id_eucatex)VALUES(?,?)");
+                stmt.setInt(1, listaPedido.get(i).getId());
+                stmt.setInt(2, listaPedido.get(i).getIdEucatex());
+    
+                stmt.executeUpdate();
+   
             }
-            
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(PedidoDAO.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(null, "Erro ao salvar pedido na Classe PedidoDAO :"+ex);
